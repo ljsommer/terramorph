d94a6bc09ab577879f1c3d23a82166da08d3c1ea
Lars Sommer <lars.j.sommer@gmail.com>
2018-03-15 14:26:31 -0700

    Alias

--- full.sh
+++ full.sh
@@ -1,35 +1,27 @@
 #! /bin/bash
 
-shopt -s expand_aliases
-
-input="${1}"
-
-if [ -z "$input" ]; then
-    log_level="INFO"
-else
-    log_level="$input"
-fi
-
-aws_default_region="us-west-2"
-
 terraform_version="0.11.3"
 
-container_aws_dir="/root/.aws/"
-container_ssh_dir="/root/.ssh/"
-
-local_aws_dir="${HOME}/.aws/"
-local_ssh_dir="${HOME}/.ssh/"
-
-container_name="terramorph"
-image_name="terramorph"
-
-echo "Building Docker image: $image_name with Terraform version: $terraform_version"
+echo "Building Docker image: terramorph with Terraform version: $terraform_version"
 docker build -t $image_name --build-arg terraform_version=$terraform_version ./docker
 
-alias terramorph="docker run -i -t --rm \
-    --name $image_name \
-    -e "AWS_DEFAULT_REGION=$aws_default_region" \
-    -e "log_level=$log_level" \
-    -v $local_aws_dir:$container_aws_dir \
-    -v $local_ssh_dir:$container_ssh_dir \
-    $image_name"
+function terramorph () {
+    docker run -i -t --rm \
+        --name terramorph \
+        -v ${HOME}/.aws/:/root/.aws/ \
+        -v ${HOME}/.ssh/:/root/.ssh/ \
+        -v $(pwd):/opt/terramorph/code/ \
+        terramorph "$1"
+}
+
+if [[ ! -s "$HOME/.bash_profile" && -s "$HOME/.profile" ]] ; then
+  profile_file="$HOME/.profile"
+else
+  profile_file="$HOME/.bash_profile"
+fi
+
+if ! grep -q 'terramorph' "${profile_file}" ; then
+  echo "Editing ${profile_file} to configure Terramorph function on terminal launch"
+  echo "function $(declare -f terramorph)"  >> "${profile_file}"
+  echo "alias tp=terramorph" >> "${profile_file}"
+fi
