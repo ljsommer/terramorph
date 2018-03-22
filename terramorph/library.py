b774debef5ad76d58ecd43cd4b31638d620e4c1e
Lars Sommer <lars.j.sommer@gmail.com>
2018-03-22 12:23:27 -0700

    In progress commit

--- terramorph/library.py
+++ terramorph/library.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+
+import logger
+from os import listdir
+from os.path import isfile, join
+
+def checkout(environment):
+    files = [f for f in listdir(environment) if isfile(join(environment, f))]
+    
 
