bf0be438e1ee709c42305fcaa5f6348fd6df91b2
Dane Lindell <dalindell@gmail.com>
2018-06-27 17:23:17 -0700

    Removed AWS_* creds from environment variables and added secrets cache to link in ~/.aws directory and import into the environment

--- terramorph/entrypoint.sh
+++ terramorph/entrypoint.sh
@@ -5,6 +5,8 @@ python="/usr/bin/python"
 terramorph="/app/src/terramorph/terramorph.py"
 working_dir="/opt/terramorph/code"
 
+if [ -f /root/.aws/terramorph ]; then source /root/.aws/terramorph; rm /root/.aws/terramorph; fi
+
 echo "Docker entrypoint file: Input read as $1"
 
 cd ${working_dir}
